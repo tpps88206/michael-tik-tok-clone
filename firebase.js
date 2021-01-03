b553a93dd5f5fb4076c70efe118df4eb04ba60ac
Sheng Chuan (Michael) Hsu <cool96482@hotmail.com>
2021-01-03 22:16:01 +0800

    first commit

---
+++ firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAYDuqaClctHIhlrVLZ4d0Bp0zd_sAM4hI",
+  authDomain: "michael-tik-tok-clone.firebaseapp.com",
+  projectId: "michael-tik-tok-clone",
+  storageBucket: "michael-tik-tok-clone.appspot.com",
+  messagingSenderId: "205659316131",
+  appId: "1:205659316131:web:59e9538ceea668399e0416",
+  measurementId: "G-QLW1TCVFF9"
+};
